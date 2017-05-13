10be8fa17f1f3ce046e3c154189813e983fa6580
John Slee <john.slee@siteminder.com>
2017-05-13 18:53:10 +1000

    rewrite entry logic

--- tt.sh
+++ tt.sh
@@ -4,10 +4,17 @@ _top_dir="$HOME/tt"
 _title_file=".tt_title"
 _date_file=".tt_date"
 _done=".tt_done"
+_tags_dir=".tt_tags"
 
 _die() {
   echo "tt: fatal: $*" > /dev/stderr
   exit 1
+}
+
+_is_macos() {
+  local uname_s
+  unames="$(uname -s)"
+  [ "$?" == 0 ] && [ "$unames" == "Darwin" ]
 }
 
 _init() {
@@ -20,17 +27,22 @@ _id() {
   echo "$$.$USER.$(date +%s).$RANDOM.$*" | openssl sha -sha256 | cut -b 1-6
 }
 
-_enter_current_or_arg() {
-  local dir
-  if [ -n "$1" ] ; then
-    dir="$_top_dir/$1"
+_try_id() {
+  if [[ "$1" =~ ^:{1}([a-f0-9]{6})$ ]] ; then
+    echo "${BASH_REMATCH[1]}"
+    true
   else
-    if [ -n "$TTID" ] ; then
-      dir="$_top_dir/$TTID"
-    else
-      _die "not in a bucket and no bucket specified"
-    fi
+    false
   fi
+}
+
+_enter_current_or_arg() {
+  local dir id
+  id="$(_try_id "$1" || _try_id "$TTID")"
+  if ! [ "$?" == 0 ] ; then
+    _die "must be in a bucket shell or specify an ID of the form :abcdef"
+  fi
+  dir="$_top_dir/$id"
   cd "$dir" || _die "can't chdir: $dir"
 }
 
@@ -83,6 +95,7 @@ _exec() {
 }
 
 _finder() {
+  _is_macos || return
   _enter_current_or_arg "$1"
   open .
 }
@@ -109,11 +122,28 @@ _new() {
   local id dir
   id="$(_id "$@")"
   dir="$_top_dir/$id"
-  mkdir -p "$dir" || _die "_new failed: $dir"
+  mkdir -p "$dir/$_tags_dir" || _die "_new failed: $dir"
   echo "$*" > "$dir/$_title_file"
-  xattr -wx "com.apple.metadata:kMDItemFinderComment" "$(_findercomment "$*")" "$dir"
+  _is_macos && xattr -wx "com.apple.metadata:kMDItemFinderComment" "$(_findercomment "$*")" "$dir"
   date +%F > "$dir/$_date_file"
-  _shell "$id"
+  _shell ":$id"
+}
+
+_tag() {
+  _enter_current_or_arg "$1"
+  shift
+  mkdir -p "$_tags_dir" || _die "failed creating tags directory: $_tags_dir"
+  # FIXME: implement macOS tagging too
+  cd "$_tags_dir"       || _die "can't chdir tags directory: $_tags_dir"
+  touch -- "$@"
+  cd ..
+}
+
+_tags() {
+  _enter_current_or_arg "$1"
+  mkdir -p "$_tags_dir" || _die "failed creating tags directory: $_tags_dir"
+  cd "$_tags_dir"       || _die "can't access tags directory: $_tags_dir"
+  /bin/ls -1 | sort
 }
 
 _ls() {
@@ -125,8 +155,8 @@ _ls() {
     find "$_top_dir" -type d -maxdepth 1 -mindepth 1 -name "$a$a$a$a$a$a" | \
       while read -r dir ; do
         id="$(basename "$dir")"
-        title="$(_title "$id")"
-        date="$(_date "$id")"
+        read -r title < "$_top_dir/$id/$_title_file"
+        read -r date < "$_top_dir/$id/$_date_file"
         done="N"
         if [ -f "$_top_dir/$id/$_done" ] ; then
           done="Y"
@@ -141,10 +171,10 @@ _help() {
   printf '\ttt new "title for new bucket"\n\n'
   printf 'usage: list buckets\n\n'
   printf '\ttt ls\n\n'
+  printf 'usage: apply tags to a bucket\n\n'
+  printf '\ttt tag :id tag1 [tag2 [..tagN]]\n\n'
   printf 'usage: get bucket metadata. Current bucket, if no ID specified.\n\n'
-  printf '\ttt (title|date|home) [id]\n\n'
-  printf 'usage: do something with a bucket. Current bucket, if no ID specified.\n\n'
-  printf '\ttt (finder|shell|done|exec) [id]\n\n'
+  printf '\ttt (title|date|home) [:id]\n\n'
   printf 'usage: garbage-collect all "done" buckets\n\n'
   printf '\ttt gc\n'
 }
@@ -153,7 +183,7 @@ _init
 
 
 case "$1" in
-  new|finder|shell|ls|'done'|keep|gc|title|date|'exec'|home)
+  new|finder|shell|ls|'done'|keep|gc|title|date|'exec'|home|tag|tags)
     cmd="$1"
     shift
     "_${cmd}" "$@"
